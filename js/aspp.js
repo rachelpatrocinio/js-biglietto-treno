cf0b5d60c257e4f36a3fe24e17ae3993a8870e02
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-03-04 13:57:44 +0100

    Added: specific strings on console.log and "else if"

--- js/aspp.js
+++ js/aspp.js
@@ -12,7 +12,7 @@ const km = parseInt(prompt("Quanti km farai?")); //num
 // calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 const ticketPrice = Math.round(km * 0.21); //num
-console.log(ticketPrice, "€");
+//console.log(ticketPrice, "€");
 
 // va applicato uno sconto del 20% per i minorenni
 const discountMinor = ticketPrice * 0.2;
@@ -27,11 +27,12 @@ const discountSenior = ticketPrice * 0.4;
 // IF utente è minorenne, il prezzo sarà scontato del 20%
 if (userAge < 18){
     //console.log(userAge, "You are a Minor");
-    console.log(ticketPrice - discountMinor, "€");
-}
-if (userAge >= 65){
+    console.log(ticketPrice, "€ Initial Price");
+    console.log(ticketPrice - discountMinor, "€ Final Price with 20% of Discount.");
+} else if(userAge >= 65){
     //console.log(userAge,"You are a Senior Citizen.");
-    console.log(ticketPrice - discountSenior, "€");
+    console.log(ticketPrice, "€ Initial Price");
+    console.log(ticketPrice - discountSenior, "€ Final Price with 40% of Discount.");
 }
 else{
     console.log(ticketPrice, "€");
