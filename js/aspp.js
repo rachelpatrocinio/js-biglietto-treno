d80f8698231cc635a0092d571e4e1f4ae869867b
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-03-04 22:34:26 +0100

    Refactor: Code improvement

--- js/aspp.js
+++ js/aspp.js
@@ -8,36 +8,28 @@ const userAge = parseInt(prompt("Quanti anni hai?")); //num
 const km = parseInt(prompt("Quanti km farai?")); //num
 //console.log(km);
 
-
 // calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
-const ticketPrice = km * 0.21; //num
+let ticketPrice = (km * 0.21).toFixed(2); //num
 //console.log(ticketPrice.toFixed(2), "€ Full Price");
 
-// va applicato uno sconto del 20% per i minorenni
-const discountMinor = ticketPrice * 0.2;
-//console.log(discountMinor.toFixed(2), "€ Discounted amount for Minors");
-
-// oppure applicato uno sconto del 40% per gli over 65.
-const discountSenior = ticketPrice * 0.4; 
-//console.log(discountSenior.toFixed(2), "€ Discounted amount for Seniors");
-
 //L’output del prezzo finale va stampato in console in forma “umana” cioè con al massimo due decimali, per indicare centesimi sul prezzo.
-
 // IF utente è minorenne, il prezzo sarà scontato del 20%
-// IF utente è over 65, il prezzo sarà scontato del 40%
+// ELSE IF utente è over 65, il prezzo sarà scontato del 40%
 // ELSE prezzo normale
 if (userAge < 18){
     //console.log(userAge, "You are a Minor");
     console.log(ticketPrice, "€ Original Price");
-    console.log((ticketPrice - discountMinor).toFixed(2), "€ Final Price with 20% of Discount.");
+    ticketPrice = (ticketPrice - ticketPrice * 0.2).toFixed(2);
+    console.log(ticketPrice, "€ Final Price with 20% of Discount.");
 } else if(userAge >= 65){
     //console.log(userAge,"You are a Senior Citizen.");
     console.log(ticketPrice, "€ Original Price");
-    console.log((ticketPrice - discountSenior).toFixed(2), "€ Final Price with 40% of Discount.");
+    ticketPrice = (ticketPrice - ticketPrice * 0.4).toFixed(2);
+    console.log(ticketPrice, "€ Final Price with 40% of Discount.");
 }
 else{
-    console.log(ticketPrice.toFixed(2), "€ Final Price");
+    console.log(ticketPrice, "€ Final Price");
 }
 
 
