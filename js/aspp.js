0841e56b168d19f54f8e6b612c449fa9ea129fee
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-03-05 10:25:42 +0100

    Added: let discount and validation of data

--- js/aspp.js
+++ js/aspp.js
@@ -2,36 +2,46 @@
 
 // DICHIARO UNA VARIABILE PER RICHIEDERE L'ETA' DELL'UTENTE
 const userAge = parseInt(prompt("Quanti anni hai?")); //num
-//console.log(userAge);
+console.log("Age:",userAge);
 
 // DICHIARO UNA VARIABILE PER RICHIEDERE I KM DA PERCORRERE
 const km = parseInt(prompt("Quanti km farai?")); //num
-//console.log(km);
+console.log("Km:",km);
 
-// calcolare il prezzo totale del viaggio, secondo queste regole:
-// il prezzo del biglietto è definito in base ai km (0.21 € al km)
-let ticketPrice = (km * 0.21).toFixed(2); //num
-//console.log(ticketPrice.toFixed(2), "€ Full Price");
+// Validazione dei dati
+// km > 0
+// userAge > 0
+// km && userAge are not NaN
 
-//L’output del prezzo finale va stampato in console in forma “umana” cioè con al massimo due decimali, per indicare centesimi sul prezzo.
-// IF utente è minorenne, il prezzo sarà scontato del 20%
-// ELSE IF utente è over 65, il prezzo sarà scontato del 40%
-// ELSE prezzo normale
-if (userAge < 18){
-    //console.log(userAge, "You are a Minor");
-    console.log(ticketPrice, "€ Original Price");
-    ticketPrice = (ticketPrice - ticketPrice * 0.2).toFixed(2);
-    console.log(ticketPrice, "€ Final Price with 20% of Discount.");
-} else if(userAge >= 65){
-    //console.log(userAge,"You are a Senior Citizen.");
-    console.log(ticketPrice, "€ Original Price");
-    ticketPrice = (ticketPrice - ticketPrice * 0.4).toFixed(2);
-    console.log(ticketPrice, "€ Final Price with 40% of Discount.");
-}
+if (km > 0 && 
+    userAge > 0 &&
+    !isNaN(km) &&
+    !isNaN(userAge)){
+    // calcolare il prezzo totale del viaggio, secondo queste regole:
+    // il prezzo del biglietto è definito in base ai km (0.21 € al km)
+    const ticketPrice = km * 0.21; //num
+    console.log("Full Price:",ticketPrice.toFixed(2), "€");
+
+    //L’output del prezzo finale va stampato in console in forma “umana” cioè con al massimo due decimali, per indicare centesimi sul prezzo.
+    // IF utente è minorenne, il prezzo sarà scontato del 20%
+    // ELSE IF utente è over 65, il prezzo sarà scontato del 40%
+    // ELSE prezzo normale
+    let discount = 0;
+    if (userAge < 18){
+        //console.log(userAge, "You are a Minor");
+        discount = ticketPrice * 0.2;
+    } else if(userAge >= 65){
+        //console.log(userAge,"You are a Senior Citizen.");
+        discount = ticketPrice * 0.4;
+    }
+
+    const finalPrice = ticketPrice - discount;
+    console.log("Final Price:", finalPrice.toFixed(2), "€");
+    }
+
 else{
-    console.log(ticketPrice, "€ Final Price");
+    alert("Not valid");
 }
-
 
 
 
